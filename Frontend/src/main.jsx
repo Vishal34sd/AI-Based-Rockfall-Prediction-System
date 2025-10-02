3401cae1dd6177e4cf5d6e88a5951f14172a2599
Vishal <dubeyvishal714@gmail.com>
2025-10-02 22:35:51 +0530

    dependencies added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,49 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import LoginPage from './components/LoginPage.jsx'
+import RegisterPage from './components/RegisterPage.jsx'
+import Dashboard from './components/DashBoard.jsx'
+import AnalyticalTab from './components/AnalyticalTab.jsx'
+import LandingPage from './components/LandingPage.jsx'
+import Alerts from './components/Alerts.jsx'
+import Report from './components/Report.jsx'
+import './App.css'
 
-import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage/>
+  },
+  {
+    path: "/login", 
+    element: <LoginPage />
+  },
+  {
+    path: "/register", 
+    element: <RegisterPage />
+  },
+  {
+    path: "/dashboard", 
+    element: <Dashboard />
+  },
+  {
+    path: "/analytic", 
+    element: <AnalyticalTab />
+  },
+  {
+    path: "/alert", 
+    element: <Alerts />
+  },
+  {
+    path: "/report", 
+    element: <Report />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+)  
